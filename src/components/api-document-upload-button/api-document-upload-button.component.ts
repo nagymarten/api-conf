3ead99a22e38dcf8c5d1d49e2cf6ac349ed4b6c4
Martin Nagy <martin.nagy@asuratechnologies.com>
2024-09-26 11:42:28 +0200

    read file data

--- src/components/api-document-upload-button/api-document-upload-button.component.ts
+++ src/components/api-document-upload-button/api-document-upload-button.component.ts
@@ -10,16 +10,39 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class ApiDocumentUploadButtonComponent {
   @ViewChild('fileInput') fileInput!: ElementRef;
 
+  fileContent: string | null = null;
+  fileLines: string[] = [];
+
+  ngAfterViewInit() {
+    // The ViewChild will be available here
+    console.log(this.fileInput);
+  }
+
   triggerFileInput() {
     this.fileInput.nativeElement.click();
   }
 
-  // Method to handle the file selection
   onFileSelected(event: any) {
     const file = event.target.files[0];
     if (file) {
-      console.log('Selected file:', file);
-      // You can now process the file here, e.g., upload it to a server
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.fileContent = e.target.result;
+        if (this.fileContent) {
+          this.fileLines = this.fileContent.split(/\r?\n/);
+          console.log('File content:', this.fileContent);
+          console.log('File lines:', this.fileLines);
+        } else {
+          console.error('File content is null or empty');
+        }
+      };
+
+      reader.onerror = (error) => {
+        console.error('Error reading file:', error);
+      };
+
+      reader.readAsText(file);
     }
   }
 }
