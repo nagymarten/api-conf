be699c12c7b7b6346dc5690f45e7c69a85d380b0
Martin Nagy <nagy.martin19@gmail.com>
2024-10-25 22:10:57 +0200

    add components to add button

--- src/app/common/components/add-scheme-button/add-scheme-button.component.ts
+++ src/app/common/components/add-scheme-button/add-scheme-button.component.ts
@@ -1,11 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
 import { OverlayPanel, OverlayPanelModule } from 'primeng/overlaypanel';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { ChipsModule } from 'primeng/chips';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { MenuItem } from 'primeng/api';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { TooltipModule } from 'primeng/tooltip';
+import { DividerModule } from 'primeng/divider';
+import { ScrollerModule } from 'primeng/scroller';
+
+interface Type {
+  name: string;
+}
 
 @Component({
   selector: 'app-add-scheme-button',
@@ -18,39 +29,63 @@ import { ButtonModule } from 'primeng/button';
     ChipsModule,
     InputTextModule,
     ButtonModule,
+    TabMenuModule,
+    DropdownModule,
+    FormsModule,
+    TooltipModule,
+    DividerModule,
+    ScrollerModule,
   ],
   templateUrl: './add-scheme-button.component.html',
   styleUrl: './add-scheme-button.component.css',
 })
-export class AddSchemeButtonComponent {
+export class AddSchemeButtonComponent implements OnInit {
   @Input() rowData: any;
   @Input() col: any;
+  @Input() apiSchemas: any;
 
   @ViewChild('op') op!: OverlayPanel;
-  members = [
-    {
-      name: 'Amy Elsner',
-      image: 'amyelsner.png',
-      email: 'amy@email.com',
-      role: 'Owner',
-    },
-    {
-      name: 'Bernardo Dominic',
-      image: 'bernardodominic.png',
-      email: 'bernardo@email.com',
-      role: 'Editor',
-    },
-    {
-      name: 'Ioni Bowcher',
-      image: 'ionibowcher.png',
-      email: 'ioni@email.com',
-      role: 'Viewer',
-    },
-  ];
+
+  responseExamples: MenuItem[] = [];
+  activeItem!: MenuItem;
+  types: Type[] | undefined;
+  selectedType: Type | undefined;
+
   showAddPropertyForm: boolean = false;
+  scrollHeight!: string;
+
+  ngOnInit() {
+    this.responseExamples = [
+      { label: 'Type', icon: 'pi pi-fw pi-tag' },
+      { label: 'Components', icon: 'pi pi-fw pi-cog' },
+      { label: 'Combine Schemas', icon: 'pi pi-fw pi-th-large' },
+    ];
+    this.types = [
+      { name: 'object' },
+      { name: 'array' },
+      { name: 'integer' },
+      { name: 'number' },
+      { name: 'string' },
+      { name: 'boolean' },
+      { name: 'enum' },
+      { name: 'dictionary' },
+    ];
+    this.activeItem = this.responseExamples[0];
+    const itemHeight = 50;
+    const maxHeight = 200; // max scrollable height
+    const calculatedHeight = this.apiSchemas.length * itemHeight;
+
+    this.scrollHeight =
+      calculatedHeight > maxHeight ? `${maxHeight}px` : `${calculatedHeight}px`;
+  }
 
   testClick(event: Event) {
     console.log('Button clicked');
     this.op.toggle(event);
+    console.log(this.apiSchemas);
+  }
+  onSchemeSelect(scheme: any) {
+    console.log('Item clicked:', scheme);
+
   }
 }
